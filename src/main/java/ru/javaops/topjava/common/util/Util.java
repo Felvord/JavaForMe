3ef6d89ea30c88dfeacb0fd02121c4579f74a58b
JavaOPs <admin@javaops.ru>
2024-04-19 20:12:27 +0300

    12_2_add_calories_meals

--- src/main/java/ru/javaops/topjava/common/util/Util.java
+++ src/main/java/ru/javaops/topjava/common/util/Util.java
@@ -2,11 +2,16 @@ package ru.javaops.topjava.common.util;
 
 import lombok.experimental.UtilityClass;
 import org.hibernate.proxy.HibernateProxy;
+import org.springframework.lang.Nullable;
 
 @UtilityClass
 public class Util {
 
     public static Class<?> getEffectiveClass(Object o) {
         return o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+    }
+
+    public static <T extends Comparable<T>> boolean isBetweenHalfOpen(T value, @Nullable T start, @Nullable T end) {
+        return (start == null || value.compareTo(start) >= 0) && (end == null || value.compareTo(end) < 0);
     }
 }
